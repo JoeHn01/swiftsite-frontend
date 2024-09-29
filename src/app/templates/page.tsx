45d99d4d18229d22d7dc3f45868e8bcf469732a0
JoeHn01 <yousef.hanafi2001@gmail.com>
2024-09-28 20:36:14 -0600

    Refactored to fetch templates from the database and display them by category

--- src/app/templates/page.tsx
+++ src/app/templates/page.tsx
@@ -1,48 +1,60 @@
+'use client'
+
+import React, { useEffect, useState } from 'react';
 import Hero from "@/components/Templates/Hero/Hero";
 import styles from "../page.module.css";
+import { LiaSpinnerSolid } from "react-icons/lia";
 import TemplateGrid from "@/components/Templates/TemplateGrid/TemplateGrid";
 
-const templates = [
-  { _id: "1", name: 'Business Pro', description: 'A modern business template for professional needs.', image: 'empty-image.jpg', category: 'Business' },
-  { _id: "2", name: 'Corporate Success', description: 'Elegant and clean template for corporate use.', image: 'empty-image.jpg', category: 'Business' },
-  { _id: "3", name: 'Startup Hub', description: 'A template designed for startups and small businesses.', image: 'empty-image.jpg', category: 'Business' },
-  { _id: "4", name: 'Agency Pro', description: 'Professional template for agencies and consultants.', image: 'empty-image.jpg', category: 'Business' },
-  { _id: "5", name: 'Finance Tracker', description: 'A sleek template for financial services and tracking.', image: 'empty-image.jpg', category: 'Business' },
-  { _id: "6", name: 'Tech Innovator', description: 'A modern template for tech companies and innovation.', image: 'empty-image.jpg', category: 'Business' },
-  { _id: "7", name: 'Freelancer Portfolio', description: 'Showcase your freelance work with this sleek portfolio template.', image: 'empty-image.jpg', category: 'Portfolio' },
-  { _id: "8", name: 'Creative Portfolio', description: 'A creative and visually appealing portfolio template.', image: 'empty-image.jpg', category: 'Portfolio' },
-  { _id: "9", name: 'Designer Showcase', description: 'Perfect for designers to present their work beautifully.', image: 'empty-image.jpg', category: 'Portfolio' },
-  { _id: "10", name: 'Photographer\'s Haven', description: 'A stunning portfolio template for photographers.', image: 'empty-image.jpg', category: 'Portfolio' },
-  { _id: "11", name: 'Artistic Expression', description: 'Ideal for artists to showcase their portfolios creatively.', image: 'empty-image.jpg', category: 'Portfolio' },
-  { _id: "12", name: 'Web Developer Portfolio', description: 'A sleek and professional portfolio for web developers.', image: 'empty-image.jpg', category: 'Portfolio' },
-  { _id: "13", name: 'Online Store', description: 'An elegant template for setting up an online store.', image: 'empty-image.jpg', category: 'E-commerce' },
-  { _id: "14", name: 'Shopify Template', description: 'Designed for Shopify, perfect for e-commerce businesses.', image: 'empty-image.jpg', category: 'E-commerce' },
-  { _id: "15", name: 'Fashion Boutique', description: 'A stylish template for fashion and apparel stores.', image: 'empty-image.jpg', category: 'E-commerce' },
-  { _id: "16", name: 'Electronics Shop', description: 'A clean and modern template for electronics sales.', image: 'empty-image.jpg', category: 'E-commerce' },
-  { _id: "17", name: 'Home Goods Store', description: 'Ideal for home and lifestyle products.', image: 'empty-image.jpg', category: 'E-commerce' },
-  { _id: "18", name: 'Jewelry Store', description: 'An elegant template for jewelry and luxury items.', image: 'empty-image.jpg', category: 'E-commerce' },
-  // { _id: "19", name: 'Restaurant Bistro', description: 'An attractive template for restaurants and bistros.', image: 'empty-image.jpg', category: 'Restaurant' },
-  // { _id: "20", name: 'Foodie Cafe', description: 'A charming template ideal for cafes and food businesses.', image: 'empty-image.jpg', category: 'Restaurant' },
-  // { _id: "21", name: 'Gourmet Kitchen', description: 'A sophisticated template for gourmet restaurants.', image: 'empty-image.jpg', category: 'Restaurant' },
-  // { _id: "22", name: 'Fast Food Delight', description: 'A fun and engaging template for fast food restaurants.', image: 'empty-image.jpg', category: 'Restaurant' },
-  // { _id: "23", name: 'Seafood Shack', description: 'A fresh and inviting template for seafood restaurants.', image: 'empty-image.jpg', category: 'Restaurant' },
-  // { _id: "24", name: 'Vegan Bistro', description: 'A modern template for vegan and vegetarian restaurants.', image: 'empty-image.jpg', category: 'Restaurant' },
-  // { _id: "25", name: 'Personal Blog', description: 'A clean and simple template for personal blogging.', image: 'empty-image.jpg', category: 'Blog' },
-  // { _id: "26", name: 'Professional Blog', description: 'A sophisticated template for professional bloggers.', image: 'empty-image.jpg', category: 'Blog' },
-  // { _id: "27", name: 'Travel Diaries', description: 'Perfect for travel bloggers to share their adventures.', image: 'empty-image.jpg', category: 'Blog' },
-  // { _id: "28", name: 'Tech Insights', description: 'A modern template for tech and gadget enthusiasts.', image: 'empty-image.jpg', category: 'Blog' },
-  // { _id: "29", name: 'Food Lovers', description: 'Ideal for food bloggers to share recipes and reviews.', image: 'empty-image.jpg', category: 'Blog' },
-  // { _id: "30", name: 'Lifestyle Journal', description: 'A stylish template for lifestyle and personal journals.', image: 'empty-image.jpg', category: 'Blog' },
-  // { _id: "31", name: 'Event Manager', description: 'An engaging template for managing and promoting events.', image: 'empty-image.jpg', category: 'Event' },
-  // { _id: "32", name: 'Conference Template', description: 'Designed for conferences, seminars, and similar events.', image: 'empty-image.jpg', category: 'Event' },
-  // { _id: "33", name: 'Wedding Planner', description: 'An elegant template for wedding planning and announcements.', image: 'empty-image.jpg', category: 'Event' },
-  // { _id: "34", name: 'Music Festival', description: 'A vibrant template for music and arts festivals.', image: 'empty-image.jpg', category: 'Event' },
-  // { _id: "35", name: 'Corporate Seminar', description: 'A professional template for corporate seminars and workshops.', image: 'empty-image.jpg', category: 'Event' },
-  // { _id: "36", name: 'Charity Gala', description: 'Designed for charity events and fundraising galas.', image: 'empty-image.jpg', category: 'Event' }
-];
+type Template = {
+  _id: string;
+  name: string;
+  description: string;
+  image: string;
+  categoryId: string;
+};
 
 export default function Templates() {
-  const categories = Array.from(new Set(templates.map(template => template.category)));
+  const [templates, setTemplates] = useState<Template[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchTemplates = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/templates');
+        if (!response.ok) {
+          throw new Error('Failed to fetch templates');
+        }
+        const data = await response.json();
+        setTemplates(data);
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError('An unknown error occurred');
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchTemplates();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="spinner-container">
+        <LiaSpinnerSolid className="spinner" />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  const categories = Array.from(new Set(templates.map(template => template.categoryId)));
 
   return (
     <main className={styles.main}>
@@ -51,7 +63,7 @@ export default function Templates() {
         <TemplateGrid
           key={category}
           title={`${category} Templates`}
-          templates={templates.filter(template => template.category === category)}
+          templates={templates.filter(template => template.categoryId === category)}
         />
       ))}
     </main>
